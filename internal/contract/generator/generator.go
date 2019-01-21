7af14da080cff7f5bcef4fffaec2bf7f03dfd862
chavacava <salvadorcavadini+github@gmail.com>
2019-01-21 15:46:27 +0100

    fix typo in contract

--- internal/contract/generator/generator.go
+++ internal/contract/generator/generator.go
@@ -74,7 +74,7 @@ func generateRequiresCode(req contract.Requires) (r ast.Stmt, e error) {
 
 //@requires fd != nil
 //@requires len(clauses) > 0
-//@ensure r == nil ==> e != nil
+//@ensures r == nil ==> e != nil
 func generateEnsuresCode(clauses []contract.Ensures, fd *ast.FuncDecl) (r ast.Stmt, e error) {
 	funcBody := []ast.Stmt{}
 
