85579129fee28cab1d1bc7365301a3b4cc47922c
chavacava <salvadorcavadini+github@gmail.com>
2019-01-22 13:46:14 +0100

    refactors GenerateCode to take io.reader/io.writer instead of filenames

--- cmd/dbc4go.go
+++ cmd/dbc4go.go
@@ -4,20 +4,59 @@ package main
 import (
 	"flag"
 	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/chavacava/dbc4go/internal/contract/generator"
 )
 
 func main() {
-	inputFilename := flag.String("i", "", "input source file")
-	outputFilename := flag.String("o", "", "output file (defaults to stdout")
+	iFilename := flag.String("i", "", "input source file")
+	oFilename := flag.String("o", "", "output file (defaults to stdout")
 	flag.Parse()
 
-	if *inputFilename == "" {
+	inputFilename := *iFilename
+	outputFilename := *oFilename
+
+	if inputFilename == "" {
 		log.Fatal("Undefined input file, please set the flag -i")
 	}
 
-	err := generator.GenerateCode(*inputFilename, *outputFilename)
+	input, err := os.Open(inputFilename)
+	if err != nil {
+		log.Fatalf("Unable to open input file: %v", err)
+	}
+
+	output := os.Stdout
+
+	if outputFilename != "" {
+		if outputFilename == inputFilename {
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			extension := strconv.Itoa(r.Intn(100))
+			outputFilename = outputFilename + "." + extension
+
+			defer func() {
+				err := os.Rename(outputFilename, inputFilename)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}()
+		}
+
+		var err error
+		output, err = os.Create(outputFilename)
+		if err != nil {
+			log.Fatalf("Unable to create output file '%s': %v", outputFilename, err)
+		}
+		defer output.Close()
+
+		log.Printf("Generating file '%s'", *oFilename)
+	}
+
+	err = generator.GenerateCode(input, output)
+	input.Close()
 	if err != nil {
 		log.Fatalf("Unable to generate code: %v", err)
 	}
