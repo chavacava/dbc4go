461af0b7c78dfac275e33b559aafe65fee138f5b
chavacava <salvador.cavadini@gmail.com>
2024-12-15 11:25:04 +0100

    adds postcondition to example

--- examples/bank_account.go
+++ examples/bank_account.go
@@ -12,46 +12,47 @@ type BankAccount struct {
 }
 
 // NewBankAccount creates an account with the given initial balance
-//@requires initialBalance >= minBalance && initialBalance <= maxBalance
-//@ensures account.balance == initialBalance
-//@ensures !account.closed
+// @requires initialBalance >= minBalance && initialBalance <= maxBalance
+// @ensures account.balance == initialBalance
+// @ensures !account.closed
 func NewBankAccount(initialBalance int) (account BankAccount) {
 	// ... implementation ...
 }
 
 // Credit the given amount to the account
-//@requires !a.closed
-//@requires amount > 0 && (a.balance + amount) <= maxBalance
-//@ensures a.balance == @old(a.balance) + amount
+// @requires !a.closed
+// @requires amount > 0 && (a.balance + amount) <= maxBalance
+// @ensures a.balance == @old(a.balance) + amount
 // Ensure other fields are unchanged:
-//@ensures !a.closed
+// @ensures !a.closed
 func (a *BankAccount) Credit(amount int) {
 	// ... implementation ...
 }
 
 // Debit the given amount from the account
-//@requires !a.closed
-//@requires amount > 0 && (a.balance - amount) >= minBalance
-//@ensures a.balance == @old(a.balance) - amount
+// @requires !a.closed
+// @requires amount > 0 && (a.balance - amount) >= minBalance
+// @ensures a.balance == @old(a.balance) - amount
 // Ensure other fields are unchanged:
-//@ensures !a.closed
+// @ensures !a.closed
 func (a *BankAccount) Debit(amount int) {
 	// ... implementation ...
 }
 
 // Close the account and returns its payout
-//@requires !a.closed
-//@ensures payout == @old(a.balance)
-//@ensures a.balance == 0
+// @requires !a.closed
+// @ensures payout == @old(a.balance)
+// @ensures a.closed
+// @ensures a.balance == 0
 func (a *BankAccount) Close() (payout int) {
 	// ... implementation ...
 }
 
 // Balance yields the balance of the account
-//@ensures balance == a.balance
+// @ensures balance == a.balance
 // Ensure other fields are unchanged:
-//@ensures a.balance == @old(a.balance)
-//@ensures a.closed == @old(a.closed)
+// @ensures a.balance == @old(a.balance)
+// @ensures a.closed == @old(a.closed)
 func (a *BankAccount) Balance() (balance int) {
 	// ... implementation ...
 }
